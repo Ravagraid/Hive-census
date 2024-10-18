d300ccc014fa3a2bd402ff36f5b9679a11ac5f91
Ravagraid <levarielravagraiddemorial@gmail.com>
2024-10-18 15:56:26 +0100

    Tweaking test

--- src/main.c
+++ src/main.c
@@ -1,33 +1,11 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
-
 #include <stdio.h>
 #include "raylib.h"
 #include "cJSON.h"
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"
+
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (450)
+#define WINDOW_TITLE "Hello Hive Scum!"
 
 int main (void)
 {
@@ -47,42 +25,37 @@ int main (void)
 	}
 	printf("%s\n", json_str);
 	fputs(json_str, fp);
-	fclose;
-		// free the JSON string and cJSON object 
-		cJSON_free(json_str);
-	cJSON_Delete(json);
 
-	// Tell the window to use vysnc and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+	SetTargetFPS(60);
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
+	Texture2D texture = LoadTexture("wabbit_alpha.png");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
 	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	while (!WindowShouldClose())
 	{
 		// drawing
 		BeginDrawing();
 
 		// Setup the backbuffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+		ClearBackground(RAYWHITE);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+		const int texture_y = SCREEN_HEIGHT / 2 - texture.width / 2;
+		DrawTexture(texture, texture_x, texture_y, WHITE);
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		const char* text = WINDOW_TITLE;
+		const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+		DrawText(json_str, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
-	UnloadTexture(wabbit);
-
+	fclose;
+	cJSON_free(json_str);
+	cJSON_Delete(json);
 	CloseWindow();
 	return 0;
 }
